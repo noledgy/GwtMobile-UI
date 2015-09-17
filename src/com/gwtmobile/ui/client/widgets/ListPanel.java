7ff4029bc5a7c8774b39f737839f39c805a93dd5
kevin <deemitnecessary@gmail.com>
2015-09-17 14:11:46 -0700

    Fixed the ListPanel issue where it wasn't restoring listeners after view being popped back into front

--- src/com/gwtmobile/ui/client/widgets/ListPanel.java
+++ src/com/gwtmobile/ui/client/widgets/ListPanel.java
@@ -32,13 +32,13 @@ import com.gwtmobile.ui.client.utils.Utils;
 
 public class ListPanel extends PanelBase implements ClickHandler, DragEventsHandler{
 
-	public enum ShowArrow { Visible, Hidden };
-	private ShowArrow _showArrow;
-	private int _selected = -1;
-	private boolean _selectable = true;
-	private double _initialX = 0.0;
-	private double _initialY = 0.0;
-	private long _lastClick = 0l;
+    public enum ShowArrow { Visible, Hidden };
+    private ShowArrow _showArrow;
+    private int _selected = -1;
+    private boolean _selectable = true;
+    private double _initialX = 0.0;
+    private double _initialY = 0.0;
+    private long _lastClick = 0l;
 
     public ListPanel() {
         //addDomHandler(this, ClickEvent.getType());
@@ -48,17 +48,17 @@ public class ListPanel extends PanelBase implements ClickHandler, DragEventsHand
 
     @Override
     protected String getDesignTimeMessage() {
-    	return "Add ListItems (recommended) or other widgets to the panel.";
+        return "Add ListItems (recommended) or other widgets to the panel.";
     }
 
     @Override
     protected void onAttach() {
-    	super.onAttach();
+        super.onAttach();
     }
 
     @Override
     protected void onDetach() {
-    	super.onDetach();
+        super.onDetach();
     }
 
     public HandlerRegistration addSelectionChangedHandler(SelectionChangedHandler handler) {
@@ -69,28 +69,33 @@ public class ListPanel extends PanelBase implements ClickHandler, DragEventsHand
     public void onLoad() {
         super.onLoad();
         //DragController.get().addDragEventsHandler(this);
+        for (Widget w : getChildren()) {
+            if (w instanceof ListItem) {
+                ((ListItem)w).setClickHandler(this);
+            }
+        }
     }
 
     @Override
     public void onUnload() {
-    	//DragController.get().removeDragEventsHandler(this);
+        //DragController.get().removeDragEventsHandler(this);
     }
 
     @Override
     public void add(Widget w) {
-    	if (w instanceof ListItem || isDesignTimeEmptyLabel(w)) {
-    		super.add(w);
-    		((ListItem)w).setClickHandler(this);
-    	}
-    	else {
-        	ListItem listItem = new ListItem();
-        	super.add(listItem);
-        	listItem.add(w);
-        	if (_showArrow == ShowArrow.Visible) {
-	        	Chevron chevron = new Chevron();
-	        	listItem.add(chevron);
-        	}
-    	}
+        if (w instanceof ListItem || isDesignTimeEmptyLabel(w)) {
+            super.add(w);
+            ((ListItem)w).setClickHandler(this);
+        }
+        else {
+            ListItem listItem = new ListItem();
+            super.add(listItem);
+            listItem.add(w);
+            if (_showArrow == ShowArrow.Visible) {
+                Chevron chevron = new Chevron();
+                listItem.add(chevron);
+            }
+        }
     }
 
     /* (non-Javadoc)
@@ -98,28 +103,29 @@ public class ListPanel extends PanelBase implements ClickHandler, DragEventsHand
      */
     @Override
     public void onClick(ClickEvent e) {
-    	// this version of on click gets triggered by items being touched.
-    	long milis = System.currentTimeMillis();
-    	if (_lastClick > 0l && milis - _lastClick < 40) {
-    		return;
-    	}
-    	_lastClick = milis;
-    	_selected = getChildren().indexOf((Widget)e.getSource());
-    	if (_selected >= 0) {
-    		ListItem item = (ListItem) getWidget(_selected);
-    		if (item.isEnabled()) {
-	            SelectionChangedEvent selectionChangedEvent = new SelectionChangedEvent(_selected,
-	            	e.getNativeEvent().getEventTarget());
-	            this.fireEvent(selectionChangedEvent);
-	            Utils.Console("Firing selection event");
-	        	item.removeStyleName(Secondary.Pressed);
-    		}
-    		_selected = -1;
+        // this version of on click gets triggered by items being touched.
+        long milis = System.currentTimeMillis();
+        if (_lastClick > 0l && milis - _lastClick < 40) {
+            return;
+        }
+        _lastClick = milis;
+        _selected = getChildren().indexOf((Widget)e.getSource());
+        if (_selected >= 0) {
+            ListItem item = (ListItem) getWidget(_selected);
+            if (item.isEnabled()) {
+                SelectionChangedEvent selectionChangedEvent = new SelectionChangedEvent(_selected,
+                    e.getNativeEvent().getEventTarget());
+                this.fireEvent(selectionChangedEvent);
+                //Utils.Console("Firing selection event");
+                item.removeStyleName(Secondary.Pressed);
+            }
+            _selected = -1;
         } else {
-        	Utils.Console("Recieved Click Event for non item");
+            //Utils.Console("Recieved Click Event for non item");
         }
     }
 
+    // Old version that depended on drag listeners
 //    @Override
 //    public void onClick(ClickEvent e) {
 //    	// when not android 5.0
@@ -147,116 +153,116 @@ public class ListPanel extends PanelBase implements ClickHandler, DragEventsHand
 //    }
 
     public void setDisplayArrow(ShowArrow show) {
-    	_showArrow = show;
-    	for (int i = 0; i < getWidgetCount(); i++) {
-    		ListItem listItem = (ListItem) getWidget(i);
-			listItem.setDisplayArrowFromParent(show);
-		}
+        _showArrow = show;
+        for (int i = 0; i < getWidgetCount(); i++) {
+            ListItem listItem = (ListItem) getWidget(i);
+            listItem.setDisplayArrowFromParent(show);
+        }
     }
 
     public ShowArrow getDisplayArrow() {
-    	return _showArrow;
+        return _showArrow;
     }
 
     public void isSelectable(boolean selectable) {
-    	this._selectable  = selectable;
+        this._selectable  = selectable;
     }
 
     public void setSelectable(boolean selectable) {
-    	this._selectable  = selectable;
+        this._selectable  = selectable;
     }
 
     public boolean getSelectable() {
-    	return _selectable;
+        return _selectable;
     }
 
     @Override
     public void onDragStart(DragEvent e) {
-    	if (_selectable) {
-	    	_selected = Utils.getTargetItemIndex(getElement(), e.getNativeEvent().getEventTarget());
-	    	if (_selected >= 0) {
-	    		//Utils.Console("drag start selected item #" +_selected);
-	    		_initialX = e.X;
-	    		_initialY = e.Y;
-	    		new Timer() {
-					@Override
-					public void run() {
-				    	if (_selected >= 0) {
-				    		ListItem item = (ListItem) getWidget(_selected);
-				    		if (item.isEnabled()) {
-					        	getWidget(_selected).addStyleName(Secondary.Pressed);
-				    		}
-				    	}
-					}
-				}.schedule(75);
-	    	}
-    	}
+        if (_selectable) {
+            _selected = Utils.getTargetItemIndex(getElement(), e.getNativeEvent().getEventTarget());
+            if (_selected >= 0) {
+                //Utils.Console("drag start selected item #" +_selected);
+                _initialX = e.X;
+                _initialY = e.Y;
+                new Timer() {
+                    @Override
+                    public void run() {
+                        if (_selected >= 0) {
+                            ListItem item = (ListItem) getWidget(_selected);
+                            if (item.isEnabled()) {
+                                getWidget(_selected).addStyleName(Secondary.Pressed);
+                            }
+                        }
+                    }
+                }.schedule(75);
+            }
+        }
     }
 
     @Override
     public void onDragMove(DragEvent e) {
-    	if (_selected >= 0) {
-        	getWidget(_selected).removeStyleName(Secondary.Pressed);
-        	if (_selected >= 0 && Math.abs(e.X - _initialX) > 40 && Math.abs(e.Y - _initialY) > 40) {
-        		// we are scrolling or swiping, so no selection
-    	    		//Utils.Console("drag move deselected item #" +_selected +" due to move");
-    	    		new Timer() {
-    					@Override
-    					public void run() {
-    				    	if (_selected >= 0) {
-    				    		ListItem item = (ListItem) getWidget(_selected);
-    				    		if (item.isEnabled()) {
-    					        	getWidget(_selected).addStyleName(Secondary.Pressed);
-    				    		}
-    				    	}
-    					}
-    				}.schedule(75);
-        		_selected = -1;
-        	}
-    	}
+        if (_selected >= 0) {
+            getWidget(_selected).removeStyleName(Secondary.Pressed);
+            if (_selected >= 0 && Math.abs(e.X - _initialX) > 40 && Math.abs(e.Y - _initialY) > 40) {
+                // we are scrolling or swiping, so no selection
+                    //Utils.Console("drag move deselected item #" +_selected +" due to move");
+                    new Timer() {
+                        @Override
+                        public void run() {
+                            if (_selected >= 0) {
+                                ListItem item = (ListItem) getWidget(_selected);
+                                if (item.isEnabled()) {
+                                    getWidget(_selected).addStyleName(Secondary.Pressed);
+                                }
+                            }
+                        }
+                    }.schedule(75);
+                _selected = -1;
+            }
+        }
     }
 
     @Override
     public void onDragEnd(DragEvent e) {
-    	if (_selected >= 0 && Math.abs(e.X - _initialX) < 40 && Math.abs(e.Y - _initialY) < 40) {
-    		// Something is selected and we aren't scrolling or swiping or something.
-        	getWidget(_selected).removeStyleName(Secondary.Pressed);
-    		//_selected = -1; need to keep the selected value for click event.
-    		//Utils.Console("drag end selected item #" +_selected);
-    		ListItem item = (ListItem) getWidget(_selected);
-        	int[] androidVersion = Utils.getAndroidVersionValues();
-        	// for android 5.0
-    		if (item.isEnabled() && null != androidVersion && androidVersion[0] >= 5) {
-	            SelectionChangedEvent selectionChangedEvent = new SelectionChangedEvent(_selected,
-	            	e.getNativeEvent().getEventTarget());
-	            this.fireEvent(selectionChangedEvent);
-	            Utils.Console("Firing selection event");
-	            new Timer() {
-	            	@Override
-	            	public void run() {
-				    	if (_selected >= 0) {
-				    		ListItem item = (ListItem) getWidget(_selected);
-				    		if (item.isEnabled()) {
-					        	getWidget(_selected).removeStyleName(Secondary.Pressed);
-				    		}
-				    	}
-	            	}
-	            }.schedule(75);
-    		} else {
-    			Utils.Console("not " + (androidVersion != null?androidVersion[0]:"null"));
-    		}
-    		_selected = -1;
-    	}
+        if (_selected >= 0 && Math.abs(e.X - _initialX) < 40 && Math.abs(e.Y - _initialY) < 40) {
+            // Something is selected and we aren't scrolling or swiping or something.
+            getWidget(_selected).removeStyleName(Secondary.Pressed);
+            //_selected = -1; need to keep the selected value for click event.
+            //Utils.Console("drag end selected item #" +_selected);
+            ListItem item = (ListItem) getWidget(_selected);
+            int[] androidVersion = Utils.getAndroidVersionValues();
+            // for android 5.0
+            if (item.isEnabled() && null != androidVersion && androidVersion[0] >= 5) {
+                SelectionChangedEvent selectionChangedEvent = new SelectionChangedEvent(_selected,
+                    e.getNativeEvent().getEventTarget());
+                this.fireEvent(selectionChangedEvent);
+                //Utils.Console("Firing selection event");
+                new Timer() {
+                    @Override
+                    public void run() {
+                        if (_selected >= 0) {
+                            ListItem item = (ListItem) getWidget(_selected);
+                            if (item.isEnabled()) {
+                                getWidget(_selected).removeStyleName(Secondary.Pressed);
+                            }
+                        }
+                    }
+                }.schedule(75);
+            } else {
+                //Utils.Console("not " + (androidVersion != null?androidVersion[0]:"null"));
+            }
+            _selected = -1;
+        }
     }
 
     public ListItem getItem(int index) {
-    	return (ListItem) getWidget(index);
+        return (ListItem) getWidget(index);
     }
 
     static class Chevron extends HTML {
-    	public Chevron() {
-    		super("<div class=\"Chevron\"><span></span><span></span></div>");
-    	}
+        public Chevron() {
+            super("<div class=\"Chevron\"><span></span><span></span></div>");
+        }
     }
 
 }
